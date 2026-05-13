b1561e4d2efea4661da712a8f7b2957ef782a1cf
Soundsphere <benedikt.rumpf@gmail.com>
2026-05-13 10:10:51 +0200

    Directory vars added

--- sffd.sh
+++ sffd.sh
@@ -2,12 +2,16 @@
 ## expects two commands. The project folder name and the target org.
 ## Target org can be ommited, though
 
+## set your directories
+d_project="$HOME/Documents/VSCode"
+d_autodocex="$HOME/Documents/Flow Explainer/salesforce-flow-docs/"
+
 ## get all flow xml files from the project folder
 if [ -z "$1" ]; then
     echo "Please provide a project folder"
 else
     echo "Starting Flow Metadata Retrieval"
-    cd ~/Documents/VSCode/$1
+    cd "$d_project"/$1
     if [ -z "$2" ]; then
         sf project retrieve start --metadata Flow
     else
@@ -16,6 +20,6 @@ else
 
     ## create the descriptions for each flow
     echo "Converting Metadata To Markdown Files"
-    cd ~/Documents/Flow\ Explainer/salesforce-flow-docs/
-    python3 -m automation_doc_exporter --project-root ~/Documents/VSCode/$1
+    cd "$d_autodocex"
+    python3 -m automation_doc_exporter --project-root "$d_project"/$1
 fi
